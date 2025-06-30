823a6e6bec98dda04a6165504e009dc02c3b8655
kamilsuchy <44306803+kamilsuchy@users.noreply.github.com>
2025-06-30 20:48:29 +0200

    usuniecie nadmiarowych danych

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,14 +33,6 @@ function App() {
   const handleConvert = async () => {
   setErrorMessage("");
 
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    event: 'convert_click',
-    event_category: 'button',
-    event_label: 'Convert button',
-  });
-
-
   if (!selectedFile || !selectedFormat) return;
   
   const formData = new FormData();
